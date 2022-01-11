ed70cdad9f853a38b9e2c9590f091fd651b0437b
Maria Ibañez <mirtraffik@gmail.com>
2022-01-11 16:01:41 +0100

    creacion de boton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+const GIFS = [
+  'https://media3.giphy.com/media/ndeihy7lvAL9C/200w.webp?cid=ecf05e47tfbgwdeyz0pp4o1xc9172m2dhd33g4clqecvwzm7&rid=200w.webp&ct=g',
+  'https://media2.giphy.com/media/uSYQsJQWEv6O4/200.webp?cid=ecf05e47tfbgwdeyz0pp4o1xc9172m2dhd33g4clqecvwzm7&rid=200.webp&ct=g'
+]
+
+const DIFFERENT_GIFS = [
+  'https://media0.giphy.com/media/6Ze96VQjmecIo/giphy.webp?cid=ecf05e47tfbgwdeyz0pp4o1xc9172m2dhd33g4clqecvwzm7&rid=giphy.webp&ct=g']
+
 function App() {
+
+  const [gifs, setGifs] =  useState (GIFS)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(singleGif =>  <img src={singleGif}/>)
+        }
+        <button onClick = {() => setGifs(DIFFERENT_GIFS)}>Cambiar gifs</button>
+       
+         
+      </section>
     </div>
   );
 }
